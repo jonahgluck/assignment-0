284217db06622a2304da3d8b03a366e241139fc8
Jonah <61380701+jonahgluck@users.noreply.github.com>
2024-09-09 23:39:10 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# add_numbers.py
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print(f"The sum is: {add_numbers(num1, num2)}")
+
